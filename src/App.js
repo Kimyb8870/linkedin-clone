3cadb66567004125d5b3f0500cf37ebc602510ed
kimyb8870 <qhd0084@gmail.com>
2020-12-29 10:57:31 +0900

    Feature: widgets section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
 import Login from "./Login";
+import Widgets from "./Widgets";
 import { useDispatch, useSelector } from "react-redux";
 import { selectUser, logout } from "./features/userSlice";
 import { auth } from "./firebase";
@@ -42,7 +43,7 @@ function App() {
         <div className="app__body">
           <Sidebar />
           <Feed />
-          {/* Widgets right  */}
+          <Widgets />
         </div>
       )}
     </div>
